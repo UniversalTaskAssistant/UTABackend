02a1c803b25cf5cf4e77fb87917eace211f19c43
MulongXie <dsh15325@163.com>
2023-12-01 15:44:31 +1100

    reorganize

---
+++ SystemConnector/SystemConnector.py
@@ -0,0 +1,10 @@
+class SystemConnector:
+    def __init__(self):
+        self.server
+        self.adb
+
+    def get_task(self):
+        return task
+
+    def get_ui(self):
+        return ui
---
+++ TaskDeclearation/TaskDeclaration.py
@@ -0,0 +1,11 @@
+class TaskDec:
+    def __init__(self):
+
+    def __inter
+
+    def __decompo
+
+    def __cls
+
+    def declear_task(self, org_task):
+        return [tasks]
