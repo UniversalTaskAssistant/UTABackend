e6d618f83099125e520a703751dfe8aadd318ba9
MulongXie <dsh15325@163.com>
2023-12-01 17:19:08 +1100

    initiate UI processing

---
+++ TaskingSystem/SystemTasker.py
@@ -0,0 +1,7 @@
+class SystemTasker:
+    def __init__(self):
+        pass
+
+    def execute_system_task(self, task):
+        return status
+
---
+++ UIProcessing/UIProcessor.py
@@ -0,0 +1,6 @@
+class UIProcessor:
+    def __init__(self):
+        pass
+
+    def process_ui(self, ui_img, ui_xml):
+        return ui_info
