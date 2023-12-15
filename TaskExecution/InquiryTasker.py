119375cb8787e6f60c6002903c5e72180e3c9295
anonymous <email@example.com>
2023-12-15 13:59:32 +1100

    change llm model name of taskinquiry

--- TaskExecution/InquiryTasker.py
+++ TaskExecution/InquiryTasker.py
@@ -4,28 +4,28 @@
 class InquiryTasker:
     def __init__(self, event_manager, system_prompt=None, **kwargs):
         self.event_manager = event_manager
-        self.event_manager.trigger_event("initialize_text_model", identifier='inquiry_tasker_model',
+        self.event_manager.trigger_event("initialize_llm_model", identifier='inquiry_tasker_model',
                                    system_prompt=system_prompt, **kwargs)
 
         # self.__model_manager = ModelManager()
-        # self.__model_manager.initialize_text_model(system_prompt=system_prompt, **kwargs)
+        # self.__model_manager.initialize_llm_model(system_prompt=system_prompt, **kwargs)
 
     def execute_inquiry_task(self, conversation):
-        # Create a new conversation message and send it to the TextModel
+        # Create a new conversation message and send it to the llmModel
         # try:
-        #     answer = self.__model_manager.create_text_conversation(conversation)
-        #     history = self.__model_manager.get_text_conversations()
+        #     answer = self.__model_manager.create_llm_conversation(conversation)
+        #     history = self.__model_manager.get_llm_conversations()
         #     return {'answer': answer, 'history': history}
         # except Exception as e:
         #     raise e
 
-        answer = self.event_manager.trigger_event("create_text_conversation", identifier='inquiry_tasker_model',
+        answer = self.event_manager.trigger_event("create_llm_conversation", identifier='inquiry_tasker_model',
                                             conversation=conversation)
-        history = self.event_manager.trigger_event("get_text_conversations", identifier='inquiry_tasker_model')
+        history = self.event_manager.trigger_event("get_llm_conversations", identifier='inquiry_tasker_model')
         return {'answer': answer, 'history': history}
 
     def reset_inquiry_tasker(self):
         # reset conversation history
-        # self.__model_manager.reset_text_conversations()
-        self.event_manager.trigger_event("reset_text_conversations", identifier='inquiry_tasker_model')
+        # self.__model_manager.reset_llm_conversations()
+        self.event_manager.trigger_event("reset_llm_conversations", identifier='inquiry_tasker_model')
 
