36959f38ac40a7628a81fa34f77ee9731e5ba2a1
MulongXie <dsh15325@163.com>
2023-12-20 12:23:20 +1100

    unit test systemconnector

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from DataStructures.config import *
+
 # cap ui raw data on the emulator
-from SystemConnection import _Device
-device = _Device()
-device.connect()
-screen_path, xml_path = device.cap_and_save_ui_screenshot_and_xml(1, 'data/device')
+from SystemConnection import SystemConnector
+sys_connector = SystemConnector()
+sys_connector.connect_adb_device()
+screen_path, xml_path = sys_connector.cap_and_save_ui_screenshot_and_xml(1, WORK_PATH + 'data/device')
 
 # initiate model manager
 from ModelManagement import ModelManager
@@ -12,6 +14,6 @@ model_mg.initialize_vision_model()
 # process ui data
 from UIProcessing.UIProcessor import UIProcessor
 ui = UIProcessor(model_manager=model_mg)
-ui_data = ui.load_ui_data(screenshot_file=screen_path, xml_file=xml_path, ui_resize=device.get_device_resolution())
+ui_data = ui.load_ui_data(screenshot_file=screen_path, xml_file=xml_path, ui_resize=sys_connector.get_device_resolution())
 ui.process_ui()
 ui_data.show_all_elements()
