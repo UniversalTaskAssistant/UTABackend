57594b11741d55a6788de29375162de0b8539392
anonymous <email@example.com>
2023-12-20 13:01:15 +1100

    change function name of AppTasker

--- TaskExecution/AppTasker.py
+++ TaskExecution/AppTasker.py
@@ -33,7 +33,7 @@ class AppTasker:
         """
         return self.action_checker.check_action(ui=ui_data, task=task, except_elements=except_elements, printlog=printlog)
 
-    def app_tasking(self, ui_data, task, except_elements=None, printlog=False):
+    def analyze_app_task(self, ui_data, task, except_elements=None, printlog=False):
         '''
         Identify the relation and action for the app-related task
         Args:
--- TaskExecution/SystemTasker.py
+++ TaskExecution/SystemTasker.py
@@ -1,5 +1,4 @@
-from .AutoTasker import _AutoTasker
-from DataStructures import _Task
+from .AutoTasker import _TaskUIActionChecker, _TaskUIRelationChecker
 
 
 class SystemTasker:
@@ -8,16 +7,39 @@ class SystemTasker:
         Initializes the SystemTasker with required paths and settings.
         Args:
             **kwargs: Keyword arguments containing paths and settings for task automation.
-                      Required keys are 'img_path', 'xml_path', 'ui_resize', and 'output_dir'.
+                      Required keys are 'ui_relation_checker', 'ui_action_checker', and 'model_manager'.
         """
-        self.__img_path = kwargs['img_path']
-        self.__xml_path = kwargs['xml_path']
-        self.__ui_resize = kwargs['ui_resize']
-        self.__output_dir = kwargs['output_dir']
+        assert 'ui_relation_checker' in kwargs and 'ui_action_checker' in kwargs and 'model_manager' in kwargs
+        self.relation_checker = _TaskUIRelationChecker(kwargs['ui_relation_checker'], kwargs['model_manager'])
+        self.action_checker = _TaskUIActionChecker(kwargs['ui_action_checker'], kwargs['model_manager'])
 
-        self.auto_tasker = _AutoTasker(self.__img_path, self.__xml_path, self.__ui_resize, self.__output_dir)
+    def check_task_ui_relation(self, ui_data, task, except_elements=None, printlog=False):
+        """
+        Checks the relation between a given ui and a task.
+        Args:
+            ui_data (UIData): ui object to be analyzed.
+            task (str): The task for which the relation is to be checked.
+            except_elements (list, optional): List of elements to exclude from consideration.
+            printlog (bool): If True, enables logging of outputs.
+        Returns:
+            _Relation between the ui and the task.
+        """
+        return self.relation_checker.check_relation(ui=ui_data, task=task, except_elements=except_elements, printlog=printlog)
 
-    def execute_system_task(self, task_id, task, printlog=False, show_operation=False, max_try=100):
+    def check_ui_action(self, ui_data, task, except_elements=None, printlog=False):
+        """
+        Determines the appropriate action and target element in the UI for a given task.
+        Args:
+            ui_data (UIData): ui object to be analyzed.
+            task (str): The task to be completed using the UI.
+            except_elements (list, optional): Elements to be excluded from consideration.
+            printlog (bool): If True, enables logging of outputs.
+        Returns:
+            _Action with the determined action and target element.
+        """
+        return self.action_checker.check_action(ui=ui_data, task=task, except_elements=except_elements, printlog=printlog)
+
+    def system_tasking(self, ui_data, task, printlog=False, show_operation=False):
         """
         Executes a system task, automating UI interactions based on the task description.
         Args:
@@ -65,7 +87,8 @@ class SystemTasker:
 
     def reset_system_tasker(self):
         """
-        Resets the internal state of the auto_tasker, including its model agents.
+        Resets the internal state of the model agents.
         """
-        self.auto_tasker.reset_model_agents()
+        self.relation_checker.reset_ui_relation_checker()
+        self.action_checker.reset_ui_action_checker()
 
