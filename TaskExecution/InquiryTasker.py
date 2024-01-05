6e9f6b8b571743b8286dcc654ded08f31bfd98d9
anonymous <email@example.com>
2024-01-05 20:42:45 +1100

    fix comments&inquiryTasker

--- TaskExecution/InquiryTasker.py
+++ TaskExecution/InquiryTasker.py
@@ -1,40 +1,19 @@
+import json
 
 
 class InquiryTasker:
     def __init__(self, model_manager):
-        self.__model_identifier = 'inquiry_tasker'
+        """
+        Initialize InquiryTasker object.
+        """
         self.__model_manager = model_manager
 
-    def initialize_agent(self):
-        """
-            Initialize llm model in model manager.
-        """
-        if self.is_agent_initialized():
-            self.delete_agent()
-        self.__model_manager.initialize_llm_model(identifier=self.__model_identifier)
-
-    def is_agent_initialized(self):
-        """
-        Check whether agent is initialized.
-        """
-        return self.__model_manager.is_llm_model_initialized(identifier=self.__model_identifier)
-
-    def delete_agent(self):
-        """
-        Remove llm model in model manager.
-        """
-        self.__model_manager.delete_llm_model(identifier=self.__model_identifier)
-
-    def execute_inquiry_task(self, conversation):
-        # Create a new conversation message and send it to the llmModel
+    def execute_inquiry_task(self, task, user_message, printlog=False):
         try:
-            answer = self.__model_manager.create_llm_conversation(self.__model_identifier, conversation)['content']
-            history = self.__model_manager.get_llm_conversations(self.__model_identifier)
-            return {'answer': answer, 'history': history}
+            task.conversation_automation.append({'role': 'user', 'content': user_message})
+            resp = self.__model_manager.send_fm_conversation(task.conversation_automation, printlog=printlog)
+            task.conversation_automation.append(resp)
+            return json.loads(resp)
         except Exception as e:
             print('error:', e)
             return False
-
-    def reset_inquiry_tasker(self):
-        # reset conversation history
-        self.__model_manager.reset_llm_conversations(self.__model_identifier)
