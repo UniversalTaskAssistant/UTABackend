61f15c0b50ab45b8ab71d35528e9ad48e947284e
MulongXie <dsh15325@163.com>
2024-01-05 17:39:19 +1100

    update config

--- DataStructures/config.py
+++ DataStructures/config.py
@@ -1,3 +1,3 @@
-WORK_PATH = 'C:/Mulong/Code/UTABackend/'
-# WORK_PATH = './'
+# WORK_PATH = 'C:/Mulong/Code/UTABackend/'
+WORK_PATH = './'
 SYSTEM_PROMPT = 'You are a mobile virtual assistant that understands and interacts with the user interface to complete a given task.'
