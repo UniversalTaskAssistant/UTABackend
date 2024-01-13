ea0446497cca53d589fb4805b592c9689887aa0f
MulongXie <dsh15325@163.com>
2024-01-13 23:09:49 +1100

    record testing

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ uta.setup_user('user1', (1080, 2280), ['whatsapp', 'facebook'])
 # 1. Declare task
 print(uta.declare_task(user_id='user2', task_id='task1', user_msg='Send "hello" to my mom'))
 print(uta.declare_task(user_id='user2', task_id='task1', user_msg='Use whatsapp'))
+
+# 2. Check action on the UI for the task
+uta.automate_task(user_id='user1', task_id='task1', ui_img_file='data/user1/task1/1.png',
+                  ui_xml_file='data/user1/task1/1.xml',
+                  printlog=False)
+
