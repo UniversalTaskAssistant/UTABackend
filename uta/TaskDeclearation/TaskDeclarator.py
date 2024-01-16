6632bffb7295650a96c232f304ab3b85b4796bbb
MulongXie <dsh15325@163.com>
2024-01-17 08:36:17 +1100

    update prompt for task classification

--- uta/TaskDeclearation/TaskDeclarator.py
+++ uta/TaskDeclearation/TaskDeclarator.py
@@ -38,25 +38,22 @@ class TaskDeclarator:
                                        '"Explanation": "The task contains two independent actions that need to be completed sequentially."}}\n' \
                                        '2. {{"Decompose"： "False", "Explanation": "This task is simple enough to be executed on the smartphone."}}\n'
 
-        self.__base_prompt_classify = 'Classify the given user task "{task}" into one of three categories for ' \
-                                      'smartphone usage: ' \
-                                      '1. General Inquiry: This category includes tasks that are general questions ' \
-                                      'not related to specific system or app functions. They can typically be ' \
-                                      'answered through internet searches. Example: "What is the weather today?" ' \
-                                      '2. System Function: These are tasks that involve system-level functions of ' \
-                                      'the smartphone. Examples include adjusting settings or using built-in ' \
-                                      'features like alarms. ' \
-                                      'Example: "Turn up the brightness", "Set an alarm at 2 pm". ' \
-                                      '3. App-Related Task: Tasks in this category require the use of specific ' \
-                                      'applications to accomplish the objective. Examples include using ride-sharing ' \
-                                      'apps or streaming services. Example: "Book a ride to my home using Uber", ' \
-                                      '"Watch a movie on YouTube". Determine which category the task "{task}" ' \
-                                      'falls into. Output your classification in JSON format with two elements: ' \
-                                      '1. "Task Type" indicating the category, and 2. "Explanation" providing a ' \
-                                      'brief rationale for your classification. ' \
-                                      'Example: {{"Task Type": "General Inquiry", "Explanation": "This task is a ' \
-                                      'general query that can be resolved through an internet search, without the ' \
-                                      'need for system-level access or specific apps."}}'
+        self.__base_prompt_classify = 'Classify the given user task "{task}" into one of three categories for smartphone usage:\n ' \
+                                      '!!!Categories:' \
+                                      '1. General Inquiry: This category includes tasks that are general questions not related to specific system or app functions. ' \
+                                      'They can typically be answered through internet searches. Example: "What is the weather today?" \n' \
+                                      '2. System Function: These are tasks that involve system-level functions of the smartphone.' \
+                                      'Examples include adjusting settings or using built-in features. ' \
+                                      'Example: "Turn up the brightness", "Enlarge the font size". \n' \
+                                      '3. App-Related Task: Requiring the use of specific applications to accomplish the objective. ' \
+                                      'Example: "Book a ride to my home using Uber", "Watch a movie on YouTube". \n' \
+                                      '!!!Determine which category the task "{task}" falls into. Output your classification in JSON format with two elements: ' \
+                                      '1. "Task Type" indicating the category;\n' \
+                                      '2. "Explanation" providing a brief rationale for your classification. \n' \
+                                      '!!!Examples:' \
+                                      '1. {{"Task Type": "General Inquiry", "Explanation": "This task is a general query that can be resolved through an internet search,' \
+                                      ' without the need for system-level access or specific apps."}}\n' \
+                                      '2. {{"Task Type: "App-Related", "Explanation": "This task needs to be done through Uber app."}}\n'
 
     def clarify_task(self, task, printlog=False):
         """
