bd55526b2bd0db32c9145979d3c8b6a3a01a78f1
anonymous <email@example.com>
2024-01-18 13:39:14 +1100

    delete config.py

--- uta/config.py
+++
@@ -1,5 +0,0 @@
-WORK_PATH = 'D:/JiamouSun/data61/projects/UTA/code/UTA_backend/'
-# WORK_PATH = 'C:/Mulong/Code/UTABackend/'
-DATA_PATH = WORK_PATH + 'data/'
-SYSTEM_PROMPT = 'You are a mobile virtual assistant that understands and interacts with the user interface to ' \
-                'complete a given task.'
