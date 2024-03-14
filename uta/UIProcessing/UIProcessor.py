7172c8943cc774b8609e0d7ab33569dc22ac3200
MulongXie <dsh15325@163.com>
2024-03-14 15:08:32 +1100

    complete refactoring of UIProcessor

--- uta/UIProcessing/UIProcessor.py
+++ uta/UIProcessing/UIProcessor.py
@@ -30,7 +30,7 @@ class UIProcessor:
             ui_data.ui_vh_json (dict): VH in a tidy json format
             ui_data.elements; ui_data.elements_leaves (list of dicts)
         """
-        print('* Pre-process XML VH *')
+        print('* Pre-process XML VH to clean-up as JSON and extract UI elements *')
         self.__ui_preprocessor.ui_vh_xml_cvt_to_json(ui_data=ui_data)
         self.__ui_preprocessor.ui_info_extraction(ui_data=ui_data)
         return ui_data
@@ -48,7 +48,7 @@ class UIProcessor:
             ui_data.element['description']: 'description' attribute in element
             ui_data.element_tree (dict): structural element tree
         """
-        print('* Analyze UI *')
+        print('* Analyze UI to generate an element tree with element descriptions *')
         self.__ui_analyser.ui_analysis_elements_description(ui_data=ui_data, ocr=ocr, cls=cls)
         self.__ui_analyser.ui_build_element_tree(ui_data)
         return ui_data
@@ -104,5 +104,6 @@ class UIProcessor:
         Returns:
             annotated_img (cv2 image): Annotated UI screenshot
         """
+        print('* Annotate elements with their IDs on the screenshot image *')
         return self.__ui_util.annotate_elements_with_id(ui_data=ui_data, only_leaves=only_leaves, show=show)
 
